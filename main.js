db29db7ee045ff663ec25854f1d604255c37a969
Rachna2030 <rachnaaulakh@GMAIL.COM>
2024-11-21 19:34:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new typed(".text",
+ {
+   Strings: ["Web Developer", "Frontend Developer", "Backend Developer"],
+   typeSpeed: 100,
+   backSpeed: 100,
+   backDelay: 1000,
+   loop: true
+
+ });
